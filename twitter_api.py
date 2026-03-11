e260ecdf4061b459a251d6ba2a76ef73a30c1806
cuentaregresiva2030cl-sketch <cuentaregresiva2030cl@gmail.com>
2026-03-11 02:47:23 -0300

    Create twitter_api.py

---
+++ twitter_api.py
@@ -0,0 +1,15 @@
+import tweepy
+import os
+
+def post_tweet_api(text,image):
+
+    auth = tweepy.OAuth1UserHandler(
+        os.environ["API_KEY"],
+        os.environ["API_SECRET"],
+        os.environ["ACCESS_TOKEN"],
+        os.environ["ACCESS_TOKEN_SECRET"]
+    )
+
+    api = tweepy.API(auth)
+
+    api.update_status_with_media(image,text)
